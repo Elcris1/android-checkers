f299a6bd6840fa152bb25cd4657fb6af2e0d4dc1
Elcris1 <eloicp10@gmail.com>
2025-04-29 11:52:23 +0200

    feat(Position.kt): passes test

---
+++ app/src/main/java/com/example/checkers/Position.kt
@@ -0,0 +1,20 @@
+package com.example.checkers
+
+class Position (var x: Int, var y: Int) {
+    fun setPosition(x: Int, y: Int) {
+        this.x = x
+        this.y = y
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Position) return false
+
+        return x == other.x &&
+                y == other.y
+    }
+
+    override fun hashCode(): Int {
+        return x.hashCode() + y.hashCode()
+    }
+}
